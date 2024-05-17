a923b30423847a88ad471a16cbe4f2d78c5b2645
Surya-Haran <165486094+Surya-Haran@users.noreply.github.com>
2024-05-17 18:12:59 +0530

    Add files via upload

---
+++ EvenNumbers.java
@@ -0,0 +1,16 @@
+package homeAssignment;
+
+public class EvenNumbers {
+	
+	
+		public static void main(String[] args) {
+			int number=10;
+			for (int i = 1; i <= number; i++) {
+				if (i%2==0) {
+					System.out.println("Even Number:" + i);
+				}
+			}
+		}
+
+	}
+
